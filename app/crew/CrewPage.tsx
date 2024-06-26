1ba7cd76861cedd1e30dc9f5ee5a408561e70c14
jmaloon <joshmaloon@gmail.com>
2024-06-26 15:46:06 -0600

    fix Crew layout

--- app/crew/CrewPage.tsx
+++ app/crew/CrewPage.tsx
@@ -24,23 +24,26 @@ export default function CrewPage({ name }: { name: CrewTab }) {
   });
 
   return (
-    <main className="m-6 sm:m-10 sm:mb-0 max-w-7xl lg:px-20 lg:mx-auto">
-      <p className="heading-xs text-white text-center sm:text-left mb-16">
+    <div className="flex flex-col gap-16 h-full p-6 sm:p-12 max-w-7xl lg:px-20 lg:mx-auto">
+      <p className="heading-xs text-white text-center sm:text-left">
         <span className="font-bold opacity-25 mr-6">02</span>
         <span className="uppercase">Meet Your Crew</span>
       </p>
-      <div className="lg:flex lg:items-center lg:gap-12">
-        <div className="flex flex-col items-center text-center gap-12 mx-auto lg:text-left lg:items-start lg:gap-20">
-          <div role="tabpanel" className="max-w-lg lg:max-w-none">
+      <div className="grow flex flex-col gap-10 justify-between lg:flex-row lg:items-center lg:gap-12">
+        <div className="flex flex-col items-center text-center gap-2 mx-auto lg:h-full lg:text-left lg:items-start lg:gap-20">
+          <div
+            role="tabpanel"
+            className="max-w-lg lg:max-w-none lg:grow lg:flex lg:flex-col lg:justify-center"
+          >
             <span className="heading-sm uppercase text-white opacity-50">
               {role}
             </span>
             <h1 className="heading-md uppercase text-white mt-2 mb-6">
               {name}
             </h1>
-            <p className="body-text text-blue">{bio}</p>
+            <p className="body-text text-blue min-h-52 sm:min-h-32">{bio}</p>
           </div>
-          <ul role="tablist" className="flex gap-4 mt-10">
+          <ul role="tablist" className="flex gap-4 lg:mb-12">
             {CREW_TABS.map((tab) => (
               <SmallPagination
                 key={tab}
@@ -50,13 +53,18 @@ export default function CrewPage({ name }: { name: CrewTab }) {
             ))}
           </ul>
         </div>
-        <picture className="block mx-auto w-72 mt-10 sm:w-full sm:max-w-lg sm:mt-0 sm:translate-y-10 lg:translate-y-0">
+        <picture className="block mx-auto w-72 sm:w-full sm:max-w-lg sm:translate-y-10  lg:translate-y-0">
           {sourcePropsList.map((props) => (
             <source key={props.type} {...props} />
           ))}
-          <img alt={`Image of ${name}`} className="w-full" />
+          <img
+            alt={`Image of ${name}`}
+            className="w-full"
+            width={539}
+            height={740}
+          />
         </picture>
       </div>
-    </main>
+    </div>
   );
 }
